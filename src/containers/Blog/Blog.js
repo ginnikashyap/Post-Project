1eb296fbf556ac475fd621aeb772c8265c0f5382
ginnikashyap <sadana_ginni@yahoo.com>
2018-08-23 21:41:22 -0400

    Updated

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
@@ -8,17 +9,23 @@ import './Blog.css';
 class Blog extends Component {
   state={
       posts:[],
-      selectedPostId: null
+      selectedPostId: null,
+      error:false
   }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
         .then(response=>{
             const posts=response.data.slice(0,4);
             const updatedPost = posts.map(post => {
                 return {...post,author:'Max'};
             });
             this.setState({posts: updatedPost})
-        })
+        }).catch(
+            error => {
+                this.setState({error:true})
+                // console.log(error);
+            }
+        )
             // console.log(response)})
     }
 
@@ -27,15 +34,20 @@ class Blog extends Component {
     }
 
     render () {
-        const posts=this.state.posts.map(
-            post=>{
-                return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => {this.postSelectedHandler(post.id)}}/>
-            }
-        )
+        let posts=<p style={{textAlign:"center"}}>Something went wrong!!!</p>
+        if(!this.state.error)
+        {
+             posts=this.state.posts.map(
+                post=>{
+                    return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => {this.postSelectedHandler(post.id)}}/>
+                }
+            );
+        }
+       
         return (
             <div>
                 <section className="Posts">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+//Adding the some default configuration and we can also split it into a new axios instance which will overwrite this global configuration.
+
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization']='AUTH TOKEN';
+axios.defaults.headers.post['Content-Type']='application/json';
+
+axios.interceptors.request.use(
+    request=>{
+        console.log(request);
+        return request;
+    },error =>{
+        console.log(error);
+        return Promise.reject(error);
+    }
+)
+
+
+axios.interceptors.response.use(
+    response=>{
+        console.log(response);
+        return response;
+    },error =>{
+        console.log(error);
+        return Promise.reject(error);
+    }
+)
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
